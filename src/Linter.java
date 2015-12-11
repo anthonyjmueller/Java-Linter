7197924654277ff5438227c0e5700906797900b6
anthonyjmueller <amnascar24@live.com>
2015-12-10 22:29:50 -0600

    adds last line must be new line: (error, needs two lines to not trip statement) adds ability to not check comments adds test file

--- src/Linter.java
+++ src/Linter.java
@@ -16,6 +16,7 @@ public class Linter {
     public static void main(String[] args) {
 
         int linecnt = 1;
+        boolean java = false;
 
         File program = new File(args[0]);
         try {
@@ -25,77 +26,105 @@ public class Linter {
                 String line = code.nextLine();
                 if (line.length() != 0) {
 
-                    if (line.length() > 80){
-                        System.out.println(linecnt + ". Line Should not be longer than 80 characters.");
-                    }
-                    //System.out.println(linecnt);
-                    Pattern curr = Pattern.compile(".*[{$|}$|;$]+");
-                    Pattern whit = Pattern.compile(".*\\s+$");
-                    Pattern dquores = Pattern.compile(".*[']{2}");
-                    Pattern singlestatement = Pattern.compile(".*[;]{1}[{\\p{Alpha}\\p{N}}\\p{Punct}}]{1}+");
-
-                    Matcher end = curr.matcher(line);
-                    Boolean semi = end.find();
-                    if (!(semi)) {
-                        System.out.println(linecnt + ". Statement should end with a semicolon.");
+                    if (!(code.hasNextLine())) {
+                        Pattern newlinelast = Pattern.compile(".{0}\\n");
+                        Matcher newlinelas = newlinelast.matcher(line);
+                        Boolean linelast = newlinelas.find();
+                        if (!(linelast)) {
+                            System.out.println("Program should end with an blank line.");
+                        }
                     }
 
-                    Matcher trawhite = whit.matcher(line);
-                    Boolean white = trawhite.find();
-                    if (white){
-                        System.out.println(linecnt + ". Statement contains trailing white space");
+                    Pattern comment = Pattern.compile("/{2}.*");
+                    Matcher comments = comment.matcher(line);
+                    Boolean comm = comments.find();
+                    if (!(java)) {
+                        Pattern javadoc = Pattern.compile("/{1}\\*{1}");
+                        Matcher javado = javadoc.matcher(line);
+                        java = javado.find();
                     }
+                    if ((!(comm)) && (!(java))) {
 
-                    Matcher dquote = dquores.matcher(line);
-                    Boolean ifdquoates = dquote.find();
-                    if (ifdquoates){
-                        System.out.println(linecnt + ". Should use single quotes");
-                    }
+                        if (line.length() > 80) {
+                            System.out.println(linecnt + ". Line Should not be longer than 80 characters.");
+                        }
 
-                    Matcher singlestatem = singlestatement.matcher(line);
-                    Boolean singlestate = singlestatem.find();
-                    if (singlestate){
-                        System.out.println(linecnt + ". Use only one statement per line");
-                    }
-
-                    Pattern bractestO = Pattern.compile("[{]+");
-                    Matcher bractestOM = bractestO.matcher(line);
-                    if (bractestOM.find()) {
                         //System.out.println(linecnt);
-                        Pattern open = Pattern.compile("[[\\p{Alpha}][\\p{Digit}][\\p{Punct}]]+[\\p{Blank}]*[{]");
+                        Pattern curr = Pattern.compile(".*[{$|}$|;$]+");
+                        Pattern whit = Pattern.compile(".*\\s+$");
+                        Pattern dquores = Pattern.compile(".*[']{2}");
+                        Pattern singlestatement = Pattern.compile(".*[;]{1}[{\\p{Alpha}\\p{N}}\\p{Punct}}]{1}+");
 
-                        Matcher openm = open.matcher(line);
-                        Boolean openb = openm.find();
-                        //System.out.println(openb);
-                        if (!(openb)) {
-                            System.out.println(linecnt + ". Open curley braces should not stand-alone.");
+                        Matcher end = curr.matcher(line);
+                        Boolean semi = end.find();
+                        if (!(semi)) {
+                            System.out.println(linecnt + ". Statement should end with a semicolon.");
+                        }
+
+                        Matcher trawhite = whit.matcher(line);
+                        Boolean white = trawhite.find();
+                        if (white) {
+                            System.out.println(linecnt + ". Statement contains trailing white space");
+                        }
+
+                        Matcher dquote = dquores.matcher(line);
+                        Boolean ifdquoates = dquote.find();
+                        if (ifdquoates) {
+                            System.out.println(linecnt + ". Should use single quotes");
+                        }
+
+                        Matcher singlestatem = singlestatement.matcher(line);
+                        Boolean singlestate = singlestatem.find();
+                        if (singlestate) {
+                            System.out.println(linecnt + ". Use only one statement per line");
+                        }
+
+                        Pattern bractestO = Pattern.compile("[{]+");
+                        Matcher bractestOM = bractestO.matcher(line);
+                        if (bractestOM.find()) {
+                            //System.out.println(linecnt);
+                            Pattern open = Pattern.compile("[[\\p{Alpha}][\\p{Digit}][\\p{Punct}]]+[\\p{Blank}]*[{]");
+
+                            Matcher openm = open.matcher(line);
+                            Boolean openb = openm.find();
+                            //System.out.println(openb);
+                            if (!(openb)) {
+                                System.out.println(linecnt + ". Open curley braces should not stand-alone.");
+                            }
+                        }
+
+                        Pattern strictequ = Pattern.compile(".*[=]{2}+");
+                        Matcher strictequs = strictequ.matcher(line);
+                        if (strictequs.find()) {
+                            //System.out.println(linecnt);
+                            Pattern strict = Pattern.compile(".*[=]{3}+");
+
+                            Matcher stricte = strict.matcher(line);
+                            Boolean equalsthree = stricte.find();
+                            //System.out.println(openb);
+                            if (!(equalsthree)) {
+                                System.out.println(linecnt + ". Should only use strict equal.");
+                            }
+                        }
+
+                        Pattern bractestC = Pattern.compile("[}]+");
+                        Matcher bractestCM = bractestC.matcher(line);
+                        if (bractestCM.find()) {
+                            Pattern close = Pattern.compile("[[\\p{Alpha}][\\p{Digit}][\\p{Punct}]]+[\\p{Blank}]*[}]");
+
+                            Matcher closem = close.matcher(line);
+                            Boolean closeb = closem.find();
+                            if (closeb) {
+                                System.out.println(linecnt + ". Closing curly braces should stand-alone.");
+                            }
                         }
                     }
-
-                    Pattern strictequ = Pattern.compile(".*[=]{2}+");
-                    Matcher strictequs = strictequ.matcher(line);
-                    if (strictequs.find()) {
-                        //System.out.println(linecnt);
-                        Pattern strict = Pattern.compile(".*[=]{3}+");
-
-                        Matcher stricte = strict.matcher(line);
-                        Boolean equalsthree = stricte.find();
-                        //System.out.println(openb);
-                        if (!(equalsthree)) {
-                            System.out.println(linecnt + ". Should only use strict equal.");
-                        }
-                    }
-
-                    Pattern bractestC = Pattern.compile("[}]+");
-                    Matcher bractestCM = bractestC.matcher(line);
-                    if (bractestCM.find()) {
-                        Pattern close = Pattern.compile("[[\\p{Alpha}][\\p{Digit}][\\p{Punct}]]+[\\p{Blank}]*[}]");
-
-                        Matcher closem = close.matcher(line);
-                        Boolean closeb = closem.find();
-                        if (closeb) {
-                            System.out.println(linecnt + ". Closing curly braces should stand-alone.");
-                        }
+                    else if (java) {
+                        Pattern njavadoc = Pattern.compile("\\*{1}/{1}");
+                        Matcher njavado = njavadoc.matcher(line);
+                        Boolean njava = njavado.find();
+                        if (njava)
+                            java = false;
                     }
                 }
                 linecnt++;
